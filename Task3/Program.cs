32b73211746d56e829dd59702157ca1fa8e2ffda
Polina <ms.maylin@inbox.ru>
2023-01-26 16:53:19 +0300

    In Task3: another solution added

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -17,3 +17,38 @@ void Output(int[] array)
 
 int[] array = new int[8];
 Output(array);
+
+// // Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+// Random rand = new Random();
+
+// void Output(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rand.Next(0, 50);
+//     }
+//     Console.WriteLine("[" + string.Join(", ", array) + "]");
+// }
+
+// int InputNumber(string str)
+// {
+//     int number;
+//     string? text;
+//     while (true)
+//     {
+//         Console.Write(str);
+//         text = Console.ReadLine();
+//         if (int.TryParse(text, out number))
+//         {
+//             break;
+//         }
+
+//         System.Console.WriteLine("Введено некорректное число, попробуйте ещё раз!");
+//     }
+//     return number;
+// }
+
+// int N = InputNumber("Введите количество элементов массива: ");
+// int[] array = new int[N];
+// Output(array);
