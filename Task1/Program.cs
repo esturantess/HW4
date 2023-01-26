1156fc08d288765452f8a2d6a8a215fd229237ef
Polina <ms.maylin@inbox.ru>
2023-01-26 15:37:40 +0300

    Task1 added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,23 @@
 //
 // 2, 4 -> 16
 
+System.Console.WriteLine("Введите первое число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+double degree = Convert.ToInt32(Console.ReadLine());
+
+double Exp(double arg1, double arg2)
+
+{
+    double result = Math.Pow(arg1, arg2);
+    return result;
+}
+
+if (degree > 0)
+{
+    System.Console.WriteLine($"Число {number} в степени {degree}: {Exp(number, degree)}");
+}
+else
+{
+    System.Console.WriteLine($"Число {degree} не натуральное.");
+}
